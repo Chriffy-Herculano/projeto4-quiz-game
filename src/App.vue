9cd713822bc1ef54e80877563e2ee8cd1c36585f
Chriffy-Herculano <chriffy123@gmail.com>
2024-05-27 22:30:08 -0300

    Continuando o projeto, instalando o axios para requisição https, fazendo requisição para api (open trivia api)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
 <template>
   <div>
-    <h1></h1>
+
+    <h1 v-html="this.question"></h1>
+
+    <input type="radio" name="options" value="true">
+    <label>Verdadeiro</label><br>
+
+    <input type="radio" name="options" value="false">
+    <label>Falso</label><br>
+
+    <button class="send" type="button">Enviar</button>
+
   </div>
 </template>
 
 <script>
+  export default {
+    name: 'App',
 
-export default {
-  name: 'App',
-}
+    //propriedade de dados
+    data() {
+      return {
+        question: undefined,
+        respostaIncorreta: undefined,
+        respostaCorreta: undefined
+      }
+    },
+
+    computed: {
+      respostas() {
+        var respostas = [...this.respostaIncorreta]; //copiando o array de respostas para minha variavel
+        respostas.push(this.respostaCorreta);
+
+        return respostas;
+      }
+    },
+    created() {
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=boolean')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.respostaIncorreta = response.data.results[0].incorrect_answers;
+        this.respostaCorreta = response.data.results[0].correct_answer;
+
+        //console.log(response.data.results)  
+      })
+    }
+
+  }
+
 </script>
 
 <style lang="scss">
@@ -18,6 +58,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
 }
 </style>
